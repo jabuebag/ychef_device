455f8dd5bf3057060fb99ba97443bb73e66533bf
jabue <sdutyang@outlook.com>
2016-04-27 16:24:30 -0700

    add menu detail page

--- www/js/menuDetail.js
+++ www/js/menuDetail.js
@@ -1,4 +1,14 @@
 // before menu detail 'page' init event listener
 myApp.onPageBeforeInit('detail_page', function (page) {
-    
+    var id = page.query.id;
+    initMenuDetailData(id);
 });
+
+function initMenuDetailData(id) {
+    var menuDetailTemplate = $$('#MenuDetailTemplate').html();
+    $$.getJSON('http://localhost:8080/listing/detailPortalJson/'+id, function (data) {
+        var menuDetailData = data;
+        var result = bindHtmlData(menuDetailTemplate, menuDetailData);
+        $$('#MenuDetailCard').html(result);
+    });
+}
